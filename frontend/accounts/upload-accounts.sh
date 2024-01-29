cf4501a4c994bacda8cc409c9eb922cfdab19712
ArkinSolomon <arkin@arkinsolomon.net>
2024-01-29 16:32:00 -0500

    I have no way to test this i hope this works

--- frontend/accounts/upload-accounts.sh
+++ frontend/accounts/upload-accounts.sh
@@ -10,7 +10,7 @@ IFS=$'\n'
 
 for EXT in $EXTENSIONS; do
   TEST_FILE=$(find . -type f -iname "*.$EXT" | head -1)
-  MIME_TYPE=$(file -b --mime-type "$TEST_FILE")
+  MIME_TYPE=$(mimetype "$TEST_FILE")
   echo "Uploading all files with a .$EXT extension with a Mime-Type of $MIME_TYPE"
   oci os object bulk-upload --src-dir ./build --namespace-name $OCI_OBJECT_STORAGE_NAMESPACE --bucket-name $ACCOUNTS_BUCKET_NAME --no-follow-symlinks --overwrite --include "*.$EXT" --parallel-upload-count 15 --content-type "$MIME_TYPE"
 done
