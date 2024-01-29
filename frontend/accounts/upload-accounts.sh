f34311b9582368180cbea1548444e33232fd9f34
ArkinSolomon <arkin@arkinsolomon.net>
2024-01-29 16:05:12 -0500

    I'm a little not smart

--- frontend/accounts/upload-accounts.sh
+++ frontend/accounts/upload-accounts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 OCI_OBJECT_STORAGE_NAMESPACE=$1
-ACCOUNTS_BUCKET_NAME=$1
+ACCOUNTS_BUCKET_NAME=$2
 
 oci os object bulk-upload --src-dir build --namespace-name $OCI_OBJECT_STORAGE_NAMESPACE --bucket-name $ACCOUNTS_BUCKET_NAME --no-follow-symlinks --overwrite --parallel-upload-count 15 --include --dry-run
