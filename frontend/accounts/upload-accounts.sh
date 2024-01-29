d8617ca901a3a750af643bfa4cc26ec014db4605
ArkinSolomon <arkin@arkinsolomon.net>
2024-01-29 16:20:17 -0500

    Test

--- frontend/accounts/upload-accounts.sh
+++ frontend/accounts/upload-accounts.sh
@@ -3,4 +3,15 @@
 OCI_OBJECT_STORAGE_NAMESPACE=$1
 ACCOUNTS_BUCKET_NAME=$2
 
-oci os object bulk-upload --src-dir build --namespace-name $OCI_OBJECT_STORAGE_NAMESPACE --bucket-name $ACCOUNTS_BUCKET_NAME --no-follow-symlinks --overwrite --parallel-upload-count 15 --include --dry-run
+find ./build -name ".DS_Store" -type f -delete
+
+EXTENSIONS=$(find ./build -type f -name '*.*' | sed 's|.*\.||' | sort -u)
+IFS=$'\n'
+
+for EXT in $EXTENSIONS; do
+  TEST_FILE=$(find . -type f -iname "*.$EXT" | head -1)
+  MIME_TYPE=$(file -b --mime-type "$TEST_FILE")
+  echo "Uploading all files with a .$EXT extension with a Mime-Type of $MIME_TYPE"
+  oci os object bulk-upload --src-dir ./build --namespace-name $OCI_OBJECT_STORAGE_NAMESPACE --bucket-name $ACCOUNTS_BUCKET_NAME --no-follow-symlinks --overwrite --include "*.$EXT" --parallel-upload-count 15 --content-type "$MIME_TYPE"
+done
+
