2ae9f6d6e079960d7fd9dedc1efa265e8f2281b8
ArkinSolomon <arkin@arkinsolomon.net>
2024-01-29 16:39:24 -0500

    Update mimetype lookup

--- frontend/accounts/upload-accounts.sh
+++ frontend/accounts/upload-accounts.sh
@@ -9,8 +9,16 @@ EXTENSIONS=$(find ./build -type f -name '*.*' | sed 's|.*\.||' | sort -u)
 IFS=$'\n'
 
 for EXT in $EXTENSIONS; do
-  TEST_FILE=$(find . -type f -iname "*.$EXT" | head -1)
-  MIME_TYPE=$(mimetype "$TEST_FILE")
+
+  if [ "$EXT" = "css" ]; then
+    MIME_TYPE="text/css"
+  elif [ "$EXT" = "css" ]; then
+    MIME_TYPE="application/javascript"
+  else 
+    TEST_FILE=$(find . -type f -iname "*.$EXT" | head -1)
+    MIME_TYPE=$(file -b --mime-type "$TEST_FILE")
+  fi
+
   echo "Uploading all files with a .$EXT extension with a Mime-Type of $MIME_TYPE"
   oci os object bulk-upload --src-dir ./build --namespace-name $OCI_OBJECT_STORAGE_NAMESPACE --bucket-name $ACCOUNTS_BUCKET_NAME --no-follow-symlinks --overwrite --include "*.$EXT" --parallel-upload-count 15 --content-type "$MIME_TYPE"
 done
